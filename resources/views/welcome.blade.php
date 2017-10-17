e3d80ed76eaed6fc11f661682dad5b90017b49a5
DeiverJC <deiver-jc@live.com>
2017-10-16 20:53:36 -0500

    Auth added

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    JobiTic
                 </div>
 
                 <div class="links">
--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,10 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
