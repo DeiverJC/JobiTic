c9d4511b518b19eca8c39e7585a413065474a248
DeiverJC <deiver-jc@live.com>
2018-02-27 00:55:54 -0500

    Stuff fixed

--- app/Location.php
+++ app/Location.php
@@ -36,7 +36,7 @@ class Location extends Model
      */
     public function city()
     {
-        return $this->belongsTo('App\city');
+        return $this->belongsTo('App\City');
     }
 
     /**
